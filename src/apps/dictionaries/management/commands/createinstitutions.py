7856c0e494a454e44612113da171212e60e68115
mitri4 <mitri4@bk.ru>
2016-07-04 17:40:02 +0300

    Refactor user command to create default institutions in dictionaries

--- src/apps/dictionaries/management/commands/createinstitutions.py
+++ src/apps/dictionaries/management/commands/createinstitutions.py
@@ -3,6 +3,7 @@
 import json
 import os
 from src.settings import ROOT_DIR
+from django.db import IntegrityError
 
 from django.core.management import BaseCommand
 
@@ -15,10 +16,11 @@ class DbFiller:
         self.institutions = None
 
     def run(self):
+        if not self.options.get('clean', False):
+            self.institutions = self._get_institutions()
+            print('Added {} institutions'.format(len(self.institutions)))
         if self.options.get('clean', False):
             self._clean_db()
-        self.institutions = self._get_institutions()
-        print('Added {} institutions'.format(len(self.institutions)))
 
     def _clean_db(self):
         Institution.objects.all().delete()
@@ -28,7 +30,10 @@ class DbFiller:
         institutions_text = open(os.path.join(ROOT_DIR, "fixtures", "institutions.json"), "r", encoding='utf-8').read()
         institutions = json.loads(institutions_text)
         for institution in institutions:
-            Institution.objects.create(name=institution["name"], type=institution["type"])
+            try:
+                Institution.objects.create(name=institution["name"], type=institution["type"])
+            except IntegrityError as e:
+                print('Error: {0} - {1}'.format(e, institution["name"]))
         return Institution.objects.all()
 
 
@@ -37,7 +42,7 @@ class Command(BaseCommand):
         parser.add_argument('--clean',
                             action='store_true',
                             dest='clean',
-                            default=True,
+                            default=False,
                             help='очистить предварительно базу')
         parser.add_argument('--create',
                             nargs='*',
