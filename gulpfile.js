66ce7562e58cff45fae5a55babccc3b4395526e1
mitri4 <mitri4@bk.ru>
2016-07-04 13:18:24 +0300

    gulf wactch sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('hello', function(callback) {
-  console.log("Hello");
-  callback();
+gulp.task('sass', function () {
+ return gulp.src('./src/static/src/sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./src/static/src/css'));
+});
+
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/static/src/sass/**/*.scss', ['sass']);
 });
