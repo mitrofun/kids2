7fcc58df782f6681ac2b2cea1bd6e041214da693
mitri4 <mitri4@bk.ru>
2017-03-15 14:02:03 +0300

    fix errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Документация «Учет детей»'
+project = u'Документация «Учёт детей»'
 copyright = u'2016-2017, mitri4'
 # author = u'mitri4'
 author = u''
@@ -109,7 +109,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = u'Документация «Учет детей»'
+htmlhelp_basename = u'Документация «Учёт детей»'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -135,7 +135,7 @@ latex_elements = {
     'preamble': '\\usepackage[utf8]{inputenc}\setcounter{tocdepth}{4}',
     'babel': '\\usepackage[russian]{babel}',
     'cmappkg': '\\usepackage{cmap}',
-    'fontenc': '\usepackage[T1,T2A]{fontenc}',
+    'fontenc': '\\usepackage[T1,T2A]{fontenc}',
     'utf8extra':'\\DeclareUnicodeCharacter{00A0}{\\nobreakspace}',
 }
 
@@ -144,7 +144,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'viewkids.tex', u'Документация «Учет детей»',
+    (master_doc, 'manual.tex', u'Документация «Учёт детей»',
      '', 'manual'),
 ]
 
@@ -154,7 +154,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'viewkids', u'Документация «Учет детей»',
+    (master_doc, 'manual', u'Документация «Учёт детей»',
      [author], 1)
 ]
 
@@ -165,8 +165,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'viewkids', u'Документация «Учет детей»',
-     author, 'viewkids', 'One line description of project.',
+    (master_doc, 'manual', u'Документация «Учёт детей»',
+     author, 'manual', 'One line description of project.',
      'Miscellaneous'),
 ]
 
